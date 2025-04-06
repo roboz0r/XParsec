4b83546f895981c10cf3a8bf00dda0d2d9835d4b
roboz0r <mental.blindfolds.on@gmail.com>
2025-04-06 17:45:52 +0200

    Improve anyOf, noneOf and pseq parsers

--- src/XParsec/Parsers.fs
+++ src/XParsec/Parsers.fs
@@ -49,6 +49,107 @@ module Parsers =
         | ValueSome(x) -> preturn x reader
         | ValueNone -> fail EndOfInput reader
 
+    /// Contains generalized parser implementations, not intended to be used directly.
+    [<RequireQualifiedAccess>]
+    module Internal =
+        let inline anyOf ([<InlineIfLambda>] contains) xs (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
+            match reader.Peek() with
+            | ValueSome c ->
+                if contains c xs then
+                    reader.Skip()
+                    preturn c reader
+                else
+                    fail (Unexpected c) reader
+            | ValueNone -> fail EndOfInput reader
+
+        let inline skipAnyOf ([<InlineIfLambda>] contains) xs (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
+            match reader.Peek() with
+            | ValueSome c ->
+                if contains c xs then
+                    reader.Skip()
+                    preturn () reader
+                else
+                    fail (Unexpected c) reader
+            | ValueNone -> fail EndOfInput reader
+
+        let inline noneOf ([<InlineIfLambda>] contains) xs (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
+            match reader.Peek() with
+            | ValueSome c ->
+                if contains c xs then
+                    fail (Unexpected c) reader
+                else
+                    reader.Skip()
+                    preturn c reader
+            | ValueNone -> fail EndOfInput reader
+
+        let inline skipNoneOf ([<InlineIfLambda>] contains) xs (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
+            match reader.Peek() with
+            | ValueSome c ->
+                if contains c xs then
+                    fail (Unexpected c) reader
+                else
+                    reader.Skip()
+                    preturn () reader
+            | ValueNone -> fail EndOfInput reader
+
+        let inline pArrayReturn (xs: 'T array) ret (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
+            let span = reader.PeekN xs.Length
+
+            if span.IsEmpty then
+                fail EndOfInput reader
+            else if MemoryExtensions.SequenceEqual(xs.AsSpan(), span) then
+                reader.SkipN xs.Length
+                preturn ret reader
+            else
+                fail (ExpectedSeq xs) reader
+
+        let inline pImmArrayReturn (xs: ImmutableArray<'T>) ret (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
+            let span = reader.PeekN xs.Length
+
+            if span.IsEmpty then
+                fail EndOfInput reader
+            else if MemoryExtensions.SequenceEqual(xs.AsSpan(), span) then
+                reader.SkipN xs.Length
+                preturn ret reader
+            else
+                fail (ExpectedSeq xs) reader
+
+        let inline pResizeArrayReturn (xs: ResizeArray<'T>) ret (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
+            let span = reader.PeekN xs.Count
+
+            if span.IsEmpty then
+                fail EndOfInput reader
+            else
+#if !FABLE_COMPILER && NET5_0_OR_GREATER
+                let span1 = System.Runtime.InteropServices.CollectionsMarshal.AsSpan xs
+
+                if MemoryExtensions.SequenceEqual(span1, span) then
+                    reader.SkipN xs.Count
+                    preturn ret reader
+                else
+                    fail (ExpectedSeq xs) reader
+#else
+                let len = xs.Count
+
+                if span.Length <> len then
+                    fail (ExpectedSeq xs) reader
+                else
+                    let mutable i = 0
+                    let mutable success = true
+
+                    while success && i < xs.Count do
+                        if span.[i] <> xs.[i] then
+                            success <- false
+
+                        i <- i + 1
+
+                    if success then
+                        reader.SkipN len
+                        preturn ret reader
+                    else
+                        fail (ExpectedSeq xs) reader
+#endif
+
     let inline satisfy ([<InlineIfLambda>] f: 'T -> bool) (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
         match reader.Peek() with
         | ValueSome(t) ->
@@ -84,68 +185,52 @@ module Parsers =
     let inline pitem (x: 'T) (reader: Reader<'T, 'State, 'Input, 'InputSlice>) = itemReturn x x reader
 
     let inline anyOf (xs: 'T seq) =
-        let xs =
             match xs with
-            | :? ('T array) as res -> res
-            | _ -> Seq.toArray xs
+        | :? ('T array) as xs -> Internal.anyOf Array.contains xs
+        | :? ('T ResizeArray) as xs -> Internal.anyOf (fun c (xs: ResizeArray<_>) -> xs.Contains c) xs
+        | :? (ImmutableArray<'T>) as xs -> Internal.anyOf (fun c (xs: ImmutableArray<_>) -> xs.Contains c) xs
+        | _ ->
+            // Convert to array for better performance
+            // as we expect the parser is called multiple times
+            let arr = Seq.toArray xs
+            Internal.anyOf Array.contains arr
 
-        fun (reader: Reader<'T, 'State, 'Input, 'InputSlice>) ->
-            match reader.Peek() with
-            | ValueSome(c) ->
-                if Array.contains c xs then
-                    reader.Skip()
-                    preturn c reader
-                else
-                    fail (Unexpected c) reader
-            | ValueNone -> fail EndOfInput reader
 
     let inline skipAnyOf (xs: 'T seq) =
-        let xs =
             match xs with
-            | :? ('T array) as res -> res
-            | _ -> Seq.toArray xs
+        | :? ('T array) as xs -> Internal.skipAnyOf Array.contains xs
+        | :? ('T ResizeArray) as xs -> Internal.skipAnyOf (fun c (xs: ResizeArray<_>) -> xs.Contains c) xs
+        | :? (ImmutableArray<'T>) as xs -> Internal.skipAnyOf (fun c (xs: ImmutableArray<_>) -> xs.Contains c) xs
+        | _ ->
+            // Convert to array for better performance
+            // as we expect the parser is called multiple times
+            let arr = Seq.toArray xs
+            Internal.skipAnyOf Array.contains arr
 
-        fun (reader: Reader<'T, 'State, 'Input, 'InputSlice>) ->
-            match reader.Peek() with
-            | ValueSome(c) ->
-                if Array.contains c xs then
-                    reader.Skip()
-                    preturn () reader
-                else
-                    fail (Unexpected c) reader
-            | ValueNone -> fail EndOfInput reader
 
     let inline noneOf (xs: 'T seq) =
-        let xs =
             match xs with
-            | :? ('T array) as res -> res
-            | _ -> Seq.toArray xs
+        | :? ('T array) as xs -> Internal.noneOf Array.contains xs
+        | :? ('T ResizeArray) as xs -> Internal.noneOf (fun c (xs: ResizeArray<_>) -> xs.Contains c) xs
+        | :? (ImmutableArray<'T>) as xs -> Internal.noneOf (fun c (xs: ImmutableArray<_>) -> xs.Contains c) xs
+        | _ ->
+            // Convert to array for better performance
+            // as we expect the parser is called multiple times
+            let arr = Seq.toArray xs
+            Internal.noneOf Array.contains arr
 
-        fun (reader: Reader<'T, 'State, 'Input, 'InputSlice>) ->
-            match reader.Peek() with
-            | ValueSome(c) ->
-                if Array.contains c xs then
-                    fail (Unexpected c) reader
-                else
-                    reader.Skip()
-                    preturn c reader
-            | ValueNone -> fail EndOfInput reader
 
     let inline skipNoneOf (xs: 'T seq) =
-        let xs =
             match xs with
-            | :? ('T array) as res -> res
-            | _ -> Seq.toArray xs
+        | :? ('T array) as xs -> Internal.skipNoneOf Array.contains xs
+        | :? ('T ResizeArray) as xs -> Internal.skipNoneOf (fun c (xs: ResizeArray<_>) -> xs.Contains c) xs
+        | :? (ImmutableArray<'T>) as xs -> Internal.skipNoneOf (fun c (xs: ImmutableArray<_>) -> xs.Contains c) xs
+        | _ ->
+            // Convert to array for better performance
+            // as we expect the parser is called multiple times
+            let arr = Seq.toArray xs
+            Internal.skipNoneOf Array.contains arr
 
-        fun (reader: Reader<'T, 'State, 'Input, 'InputSlice>) ->
-            match reader.Peek() with
-            | ValueSome(c) ->
-                if Array.contains c xs then
-                    fail (Unexpected c) reader
-                else
-                    reader.Skip()
-                    preturn () reader
-            | ValueNone -> fail EndOfInput reader
 
     let inline anyInRange xMin xMax (reader: Reader<'T, 'State, 'Input, 'InputSlice>) =
         match reader.Peek() with
@@ -167,43 +252,31 @@ module Parsers =
                 fail (Unexpected c) reader
         | ValueNone -> fail EndOfInput reader
 
-    let pseq (xs: 'T seq) =
-        let xs =
-            match xs with
-#if !FABLE_COMPILER
-            | :? (ImmutableArray<'T>) as res -> res
-#endif
-            | _ -> ImmutableArray.CreateRange xs
+    /// Succeeds if the next items in the input are equal to the given items, and consumes them.
+    /// Returns the items, otherwise fails with the Expected items.
+    let pseq (xs: #seq<'T>) =
+        match box xs with
+        | :? ('T array) as arr -> Internal.pArrayReturn arr xs
+        | :? ('T ResizeArray) as arr -> Internal.pResizeArrayReturn arr xs
+        | :? (ImmutableArray<'T>) as arr -> Internal.pImmArrayReturn arr xs
+        | _ ->
+            // Convert to array for better performance
+            // as we expect the parser is called multiple times
+            let arr = Seq.toArray xs
+            Internal.pArrayReturn arr xs
 
-        fun (reader: Reader<'T, 'State, 'Input, 'InputSlice>) ->
-            let span = reader.PeekN xs.Length
-
-            if span.IsEmpty then
-                fail (ExpectedSeq xs) reader
-            else if MemoryExtensions.SequenceEqual(xs.AsSpan(), span) then
-                reader.SkipN xs.Length
-                preturn xs reader
-            else
-                fail (ExpectedSeq xs) reader
-
-    let pseqReturn (xs: 'T seq) result =
-        let xs =
-            match xs with
-#if !FABLE_COMPILER
-            | :? (ImmutableArray<'T>) as res -> res
-#endif
-            | _ -> ImmutableArray.CreateRange xs
-
-        fun (reader: Reader<'T, 'State, 'Input, 'InputSlice>) ->
-            let span = reader.PeekN xs.Length
-
-            if span.IsEmpty then
-                fail (ExpectedSeq xs) reader
-            else if MemoryExtensions.SequenceEqual(xs.AsSpan(), span) then
-                reader.SkipN xs.Length
-                preturn result reader
-            else
-                fail (ExpectedSeq xs) reader
+    /// Succeeds if the next items in the input are equal to the given items, and consumes them.
+    /// Returns the result, otherwise fails with the Expected items.
+    let pseqReturn (xs: #seq<'T>) result =
+        match box xs with
+        | :? ('T array) as arr -> Internal.pArrayReturn arr result
+        | :? ('T ResizeArray) as arr -> Internal.pResizeArrayReturn arr result
+        | :? (ImmutableArray<'T>) as arr -> Internal.pImmArrayReturn arr result
+        | _ ->
+            // Convert to array for better performance
+            // as we expect the parser is called multiple times
+            let arr = Seq.toArray xs
+            Internal.pArrayReturn arr result
 
 /// Holds a mutable reference to a parser allowing the creation of forward-reference or mutually recursive parsers
 [<Sealed>]
