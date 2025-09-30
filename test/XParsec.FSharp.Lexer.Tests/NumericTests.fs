88dbc3e3bbd894097b39b7c597a8d01a65d1688f
roboz0r <mental.blindfolds.on@gmail.com>
2025-09-30 00:05:28 -0500

    Add tests for keyword and numeric parsers

---
+++ test/XParsec.FSharp.Lexer.Tests/NumericTests.fs
@@ -0,0 +1,281 @@
+module XParsec.FSharp.Lexer.Tests.NumericTests
+
+open System
+open System.Text
+
+open Expecto
+
+open XParsec
+open XParsec.Parsers
+open XParsec.CharParsers
+open XParsec.FSharp.Lexer
+open XParsec.FSharp.Lexer.Lexing
+
+let intPartCases =
+    [|
+        // Hexadecimal
+        // Simple cases
+        ("0x42", NumericBase.Hex)
+        ("0XFF", NumericBase.Hex)
+        ("0x0", NumericBase.Hex)
+        ("0x1", NumericBase.Hex)
+
+        // Case-insensitivity
+        ("0xabcdef", NumericBase.Hex)
+        ("0XABCDEF", NumericBase.Hex)
+        ("0xDeAdBeEf", NumericBase.Hex)
+
+        // With underscores
+        ("0x123_456", NumericBase.Hex)
+        ("0XDEAD_BEEF", NumericBase.Hex)
+        ("0xFF_FF__FF", NumericBase.Hex) // Multiple consecutive underscores
+        ("0x9_", NumericBase.Hex) // Trailing underscore should be ignored by the numeric part
+
+        // Octal
+        // Simple cases
+        ("0o777", NumericBase.Octal)
+        ("0O123", NumericBase.Octal)
+        ("0o0", NumericBase.Octal)
+        ("0o1", NumericBase.Octal)
+
+        // With underscores
+        ("0o123_456", NumericBase.Octal)
+        ("0O7_1_1", NumericBase.Octal)
+        ("0o1__2__3", NumericBase.Octal)
+        ("0o7_", NumericBase.Octal)
+
+        // Binary
+        // Simple cases
+        ("0b1010", NumericBase.Binary)
+        ("0B1111", NumericBase.Binary)
+        ("0b0", NumericBase.Binary)
+        ("0b1", NumericBase.Binary)
+
+        // With underscores
+        ("0b1000_0001", NumericBase.Binary)
+        ("0B1110__0101", NumericBase.Binary)
+        ("0b1_0_1_0", NumericBase.Binary)
+        ("0b1_", NumericBase.Binary)
+
+        // Should resolve to Decimal (only contains 0 or 1 but no base prefix)
+        ("0", NumericBase.Decimal)
+        ("1", NumericBase.Decimal)
+        ("10110", NumericBase.Decimal)
+        ("111_000", NumericBase.Decimal)
+
+        // Should resolve to Decimal (contains digits 2-7)
+        ("7", NumericBase.Decimal)
+        ("12345670", NumericBase.Decimal)
+        ("1_2_3_4_5_6_7", NumericBase.Decimal)
+        ("777", NumericBase.Decimal)
+
+        // Should resolve to Decimal (contains 8 or 9)
+        ("9", NumericBase.Decimal)
+        ("8", NumericBase.Decimal)
+        ("123945", NumericBase.Decimal)
+        ("1_000_000", NumericBase.Decimal)
+        ("9876543210", NumericBase.Decimal)
+
+    |]
+
+// regexp digit = [0-9]
+// regexp hexdigit = digit | [A-F] | [a-f]
+// regexp octaldigit = [0-7]
+// regexp bitdigit = [0-1]
+
+// regexp int =
+//     | digit+                                     For example, 34
+//     | int _ int                                  For example, 123_456_789
+
+// regexp hexint =
+//     | hexdigit+                                  For example, 1f
+//     | hexint _ hexint                            For example, abc_456_78
+
+// regexp octalint =
+//     | octaldigit+                                For example, 34
+//     | octalint _ octalint                        For example, 123_4_711
+
+// regexp bitint =
+//     | bitdigit+                                  For example, 11
+//     | bitint _ bitint                            For example, 1110_0101_0100
+
+// regexp xint =
+//     | 0 (x|X) hexint                             For example, 0x22_1f
+//     | 0 (o|O) octalint                           For example, 0o42
+//     | 0 (b|B) bitint                             For example, 0b10010
+
+// token sbyte = ( int | xint ) 'y'                 For example, 34y
+// token byte = ( int | xint ) ' uy'                For example, 34uy
+// token int16 = ( int | xint ) 's'                 For example, 34s
+// token uint16 = ( int | xint ) 'us'               For example, 34us
+// token int32 = ( int | xint ) 'l'                 For example, 34l
+// token uint32 = ( int | xint ) 'ul'               For example, 34ul
+//              | ( int | xint ) 'u'                For example, 34u
+// token nativeint = ( int | xint ) 'n'             For example, 34n
+// token unativeint = ( int | xint ) 'un'           For example, 34un
+// token int64 = ( int | xint ) 'L'                 For example, 34L
+// token uint64 = ( int | xint ) 'UL'               For example, 34UL
+//              | ( int | xint ) 'uL'               For example, 34uL
+
+// token float =
+//     int . int?
+//     int (. int?)? (e|E) (+|-)? int
+
+// token ieee32 =
+//     | float [Ff]                                 For example, 3.0F or 3.0f
+//     | xint 'lf'                                  For example, 0x00000000lf
+// token ieee64 =
+//     | float                                      For example, 3.0
+//     | xint 'LF'                                  For example, 0x0000000000000000LF
+
+// token bignum = int ('Q' | ' R' | 'Z' | 'I' | 'N' | 'G')
+//                                                  For example, 34742626263193832612536171N
+
+// token decimal = ( float | int ) [Mm]
+
+let integerLiteralCases =
+    [|
+        // Decimal integer literals
+        ("0", Token.NumInt32)
+        ("42", Token.NumInt32)
+        ("123456", Token.NumInt32)
+        ("1_000_000", Token.NumInt32)
+        ("2147483647", Token.NumInt32) // Max int32
+
+        // SByte
+        ("127y", Token.NumSByte)
+        // TODO: Enable when negative literals are supported
+        // ("-128y", Token.NumSByte)
+        ("0y", Token.NumSByte)
+        ("0x7Fy", Token.NumSByteHex)
+        ("0b01111111y", Token.NumSByteBinary)
+        ("0o177y", Token.NumSByteOctal)
+
+        // Byte
+        ("255uy", Token.NumByte)
+        ("0uy", Token.NumByte)
+        ("0xFFuy", Token.NumByteHex)
+        ("0b11111111uy", Token.NumByteBinary)
+        ("0o377uy", Token.NumByteOctal)
+
+        // Int16
+        ("32767s", Token.NumInt16)
+        // ("-32768s", Token.NumInt16)
+        ("0s", Token.NumInt16)
+        ("0x7FFFs", Token.NumInt16Hex)
+        ("0b0111111111111111s", Token.NumInt16Binary)
+        ("0o77777s", Token.NumInt16Octal)
+
+        // UInt16
+        ("65535us", Token.NumUInt16)
+        ("0us", Token.NumUInt16)
+        ("0xFFFFus", Token.NumUInt16Hex)
+        ("0b1111111111111111us", Token.NumUInt16Binary)
+        ("0o177777us", Token.NumUInt16Octal)
+
+        // Int32
+        ("2147483647l", Token.NumInt32)
+        // ("-2147483648l", Token.NumInt32)
+        ("0l", Token.NumInt32)
+        ("0x7FFFFFFFl", Token.NumInt32Hex)
+        ("0b01111111111111111111111111111111l", Token.NumInt32Binary)
+        ("0o17777777777l", Token.NumInt32Octal)
+
+        // UInt32
+        ("4294967295ul", Token.NumUInt32)
+        ("0ul", Token.NumUInt32)
+        ("0xFFFFFFFFul", Token.NumUInt32Hex)
+        ("0b11111111111111111111111111111111ul", Token.NumUInt32Binary)
+        ("0o37777777777ul", Token.NumUInt32Octal)
+        ("42u", Token.NumUInt32)
+        ("0x2Au", Token.NumUInt32Hex)
+        ("0b101010u", Token.NumUInt32Binary)
+        ("0o52u", Token.NumUInt32Octal)
+
+        // NativeInt
+        ("123n", Token.NumNativeInt)
+        ("0x7Fn", Token.NumNativeIntHex)
+        ("0b1010n", Token.NumNativeIntBinary)
+        ("0o77n", Token.NumNativeIntOctal)
+
+        // UNativeInt
+        ("123un", Token.NumUNativeInt)
+        ("0x7Fun", Token.NumUNativeIntHex)
+        ("0b1010un", Token.NumUNativeIntBinary)
+        ("0o77un", Token.NumUNativeIntOctal)
+
+        // Int64
+        ("9223372036854775807L", Token.NumInt64)
+        // ("-9223372036854775808L", Token.NumInt64)
+        ("0L", Token.NumInt64)
+        ("0x7FFFFFFFFFFFFFFFL", Token.NumInt64Hex)
+        ("0b0111111111111111111111111111111111111111111111111111111111111111L", Token.NumInt64Binary)
+        ("0o777777777777777777777L", Token.NumInt64Octal)
+
+        // UInt64
+        ("18446744073709551615UL", Token.NumUInt64)
+        ("0UL", Token.NumUInt64)
+        ("0xFFFFFFFFFFFFFFFFUL", Token.NumUInt64Hex)
+        ("0b1111111111111111111111111111111111111111111111111111111111111111UL", Token.NumUInt64Binary)
+        ("0o1777777777777777777777UL", Token.NumUInt64Octal)
+        ("42uL", Token.NumUInt64)
+        ("0x2AuL", Token.NumUInt64Hex)
+        ("0b101010uL", Token.NumUInt64Binary)
+        ("0o52uL", Token.NumUInt64Octal)
+
+        // BigNum (arbitrary precision integer) literals
+        // Note: Hex, Octal, and Binary forms are not supported for BigNum in this implementation
+        ("123Q", Token.NumBigInteger)
+        ("456R", Token.NumBigInteger)
+        ("789Z", Token.NumBigInteger)
+        ("101112I", Token.NumBigInteger)
+        ("131415N", Token.NumBigInteger)
+        ("161718G", Token.NumBigInteger)
+        ("0Q", Token.NumBigInteger)
+        ("0R", Token.NumBigInteger)
+        ("0Z", Token.NumBigInteger)
+        ("0I", Token.NumBigInteger)
+        ("0N", Token.NumBigInteger)
+        ("0G", Token.NumBigInteger)
+    |]
+
+// Tests for integer literals with different bases and formats
+
+
+[<Tests>]
+let tests1 =
+    testList
+        "Integer Fragment Tests"
+        [
+            for input, expectedBase in intPartCases do
+                test $"Lexing numeric literal '{input}'" {
+                    let reader = Reader.ofString input (LexBuilder.init ())
+
+                    match NumericLiterals.pXIntBase (StringBuilder()) reader with
+                    | Ok { Parsed = (value, actualBase) } ->
+                        "" |> Expect.equal actualBase expectedBase
+                        "" |> Expect.equal (value.ToString()) (input.Replace("_", "")) // Underscores are ignored in numeric literals
+                        "" |> Expect.equal reader.Index (int64 (input.TrimEnd('_').Length))
+                    | Error err -> failtestf "Lexing failed: %A" err
+                }
+        ]
+
+
+[<Tests>]
+let tests2 =
+    testSequenced
+    <| testList
+        "Integer Literal Tests"
+        [
+            for input, expectedToken in integerLiteralCases do
+                test $"Lexing numeric literal '{input}'" {
+                    // printfn "Testing numeric literal: '%s' (Token: %O)" input expectedToken
+                    let expected =
+                        [
+                            PositionedToken.Create(expectedToken, 0)
+                            PositionedToken.Create(Token.EOF, input.Length)
+                        ]
+
+                    testLexed input expected
+                }
+        ]
---
+++ test/XParsec.FSharp.Lexer.Tests/TestHelpers.fs
@@ -0,0 +1,159 @@
+[<AutoOpen>]
+module XParsec.FSharp.Lexer.Tests.TestHelpers
+
+open System
+open System.IO
+
+open Expecto
+
+open XParsec
+open XParsec.CharParsers
+open XParsec.FSharp.Lexer
+open XParsec.Parsers
+open XParsec.FSharp.Lexer.Lexing
+
+let truncateMiddle (maxLength: int) (s: string) =
+    if s.Length <= maxLength then
+        s
+    else
+        let partLength = (maxLength - 3) / 2
+        let firstPart = s.Substring(0, partLength)
+        let secondPart = s.Substring(s.Length - partLength, partLength)
+        firstPart + "..." + secondPart
+
+let escapeSpecialCharacters (s: string) =
+    s.Replace("\n", "\\n").Replace("\r", "\\r").Replace("\t", "\\t")
+
+let printLexed (input: string) (x: Lexed) =
+    let rec f i =
+        if i < x.Length then
+            let pt = x.Tokens[i]
+            let p = int pt.StartIndex
+
+            let p1 =
+                x.Tokens
+                |> Seq.tryItem (i + 1)
+                |> Option.map (fun pt1 -> int pt1.StartIndex)
+                |> Option.defaultValue input.Length
+
+            match pt.TokenWithoutCommentFlags with
+            | t when t.IsOperator ->
+                let id = input.AsSpan(p, p1 - p).ToString()
+                printfn "%d, %A (%s)" p t id
+            | Token.InvalidOperator
+            | Token.Identifier
+            | Token.OtherUnlexed
+            | Token.EscapePercent
+            | Token.EscapeLBrace
+            | Token.EscapeRBrace
+            | Token.FormatPlaceholder
+            | Token.InvalidFormatPlaceholder
+            | Token.InvalidFormatPercents
+            | Token.InterpolatedExpressionOpen
+            | Token.InterpolatedExpressionClose
+            | Token.Interpolated3StringOpen
+            | Token.Interpolated3StringClose
+            | Token.InterpolatedStringOpen
+            | Token.InterpolatedStringClose
+            | Token.VerbatimInterpolatedStringOpen
+            | Token.VerbatimInterpolatedStringClose as t ->
+                let id = input.AsSpan(p, p1 - p).ToString()
+                printfn "%d, %A (%s)" p t id
+            | Token.StringLiteral
+            | Token.UnterminatedStringLiteral
+            | Token.VerbatimStringLiteral
+            | Token.UnterminatedVerbatimStringLiteral
+            | Token.String3Literal
+            | Token.UnterminatedString3Literal
+            | Token.UnterminatedInterpolatedString
+            | Token.InterpolatedStringFragment
+            | Token.Interpolated3StringFragment
+            | Token.VerbatimInterpolatedStringFragment as t ->
+                let id = input.AsSpan(p, p1 - p).ToString()
+                let id = escapeSpecialCharacters id
+                let id = truncateMiddle 40 id
+                printfn "%d, %A (%s)" p t id
+
+            | t -> printfn "%d, %A" p t
+
+            f (i + 1)
+
+    f 0
+
+let writeLexed (path: string) (x: Lexed) =
+    use writer = new StreamWriter(path)
+
+    let rec f i =
+        if i < x.Length then
+            writer.WriteLine($"%d{int x.Tokens[i].StartIndex}, %A{x.Tokens[i].Token}")
+            f (i + 1)
+
+    f 0
+
+let pToken =
+    // Get enum cases
+    typeof<Token>.GetEnumValues()
+    |> Seq.cast<Token>
+    |> Seq.toArray
+    |> Array.map (fun case ->
+        // printfn "Creating parser for %O" case
+        pstring (case.ToString()) >>% case
+    )
+    |> fun ps -> choiceL ps "token"
+
+
+let readLexed (path: string) =
+    let parser =
+        many (pipe4 pint32 (pstring ", ") pToken newline (fun pos _ tok _ -> PositionedToken.Create(tok, pos)))
+        .>> eof
+
+    let text = File.ReadAllText path
+
+    match parser (Reader.ofString text ()) with
+    | Ok { Parsed = result } -> result |> List.ofSeq
+    | Error err -> failwithf "Error reading lexed file: %A" err
+
+let testLexed (input: string) (expected: _ list) =
+    // for i in 0 .. 100000 do
+    // Run multiple times to catch any state issues
+    match lexString input with
+    | Ok { Parsed = lexed } ->
+        try
+            "" |> Expect.equal (lexed |> Lexed.asSeq |> List.ofSeq) expected
+        with ex ->
+            printfn "An error occurred: %s" (ex.Message)
+            printLexed input lexed
+            reraise ()
+    | Error err ->
+        printfn "Lexing failed: %A" err
+        failwith "Lexing failed"
+
+let testDataDir =
+    lazy DirectoryInfo(IO.Path.Combine(__SOURCE_DIRECTORY__, "..", "data")).FullName
+
+let testData =
+    lazy
+        let dir = testDataDir.Value
+        IO.Directory.GetFiles(dir, "*.fs")
+
+let testLexFile (filePath: string) =
+    let input = File.ReadAllText filePath
+    let expectedPath = filePath + ".lexed"
+
+    if not (File.Exists expectedPath) then
+        // Doesn't exist so create it
+        match lexString input with
+        | Ok { Parsed = lexed } ->
+            printfn "Expected lexed file does not exist at %s" expectedPath
+            printfn "-------------\nInput was:\n%s" input
+            printfn "-------------\nLexed output is:\n"
+            printLexed input lexed
+            printfn "-------------"
+            writeLexed expectedPath lexed
+            failtestf "Created expected lexed file at %s, please verify it is correct" expectedPath
+        | Error err ->
+            printfn "Lexing failed: %A" err
+            failwith "Lexing failed"
+    else
+        let expected = readLexed expectedPath
+        testLexed input expected
