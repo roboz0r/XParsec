28f5a5db8692778a9f98606bc80f4d8e74cb5fef
roboz0r <mental.blindfolds.on@gmail.com>
2025-10-02 21:55:03 -0500

    Fully lex some basic files

--- test/data/06_triple_dollar_with_format_specifier.fs
+++ test/data/06_triple_dollar_with_format_specifier.fs
@@ -1,5 +1,4 @@
 // Triple-dollar with format specifier and percent signs in text.
 let c = 789
-let s = 
-    $$$"""This is a test with a % sign and a %%%d{{{c}}}"""
+let s = $$$"""This is a test with a % sign and a %%%d{{{c}}}"""
 printfn "%s" s
