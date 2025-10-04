e0cd307f854af888887cce0780a483f64a50b035
roboz0r <mental.blindfolds.on@gmail.com>
2025-10-04 15:04:36 -0500

    Preprocess lexical blocks in the lexer

---
+++ test/data/23_data_types.fs
@@ -0,0 +1,16 @@
+// Group data with Records
+type SuccessfulWithdrawal =
+    { Amount: decimal
+      Balance: decimal }
+
+type FailedWithdrawal =
+    { Amount: decimal
+      Balance: decimal
+      IsOverdraft: bool }
+
+// Use discriminated unions to represent data of 1 or more forms
+type WithdrawalResult =
+    | Success of SuccessfulWithdrawal
+    | InsufficientFunds of FailedWithdrawal
+    | CardExpired of System.DateTime
+    | UndisclosedFailure
