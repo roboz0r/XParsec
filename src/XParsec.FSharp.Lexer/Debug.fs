beb4a88cf5bbb15db48860bfc970188a289df223
roboz0r <mental.blindfolds.on@gmail.com>
2025-11-30 15:33:53 -0600

    Reworking Token definition

--- src/XParsec.FSharp.Lexer/Debug.fs
+++ src/XParsec.FSharp.Lexer/Debug.fs
@@ -1,20 +1,21 @@
-module rec XParsec.FSharp.Parser.Debug
+module rec XParsec.FSharp.Debug
 
 open System
 open System.CodeDom.Compiler
 
 open XParsec.FSharp.Lexer
+open XParsec.FSharp.Parser
 
 let printTokenMin (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (token: SyntaxToken) =
     match token.Index with
-    | TokenIndex.Regular iT -> tw.Write($"{token.Token}({iT})")
-    | TokenIndex.Virtual -> tw.Write($"{token.Token}(<virt>)")
+    | TokenIndex.Regular iT -> tw.Write($"{token.PositionedToken}({iT}<token>)")
+    | TokenIndex.Virtual -> tw.Write($"{token.PositionedToken}(<virt>)")
 
-let printTokenFull (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (token: SyntaxToken) =
-    match token.Index with
+let printTokenFull (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (synTok: SyntaxToken) =
+    match synTok.Index with
     | TokenIndex.Regular iT ->
         let t1 = lexed.Tokens.[iT + 1<_>]
-        let i = token.StartIndex
+        let i = synTok.StartIndex
         let i1 = t1.StartIndex
         let iEnd = i1 - 1L
         let len = iEnd - i
@@ -25,20 +26,18 @@ let printTokenFull (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (toke
             else
                 input.[int i .. int (i1 - 1L)]
 
-        tw.Write($"{token.Token}({iT}) '{tokenStr}'")
-    | TokenIndex.Virtual -> tw.Write($"{token.Token}(<virt>)")
+        let tokenStr =
+            tokenStr.Replace("\n", "\\n").Replace("\r", "\\r").Replace("\t", "\\t")
 
-let printConstant
-    (tw: IndentedTextWriter)
-    (input: string)
-    (lexed: Lexed)
-    (x: XParsec.FSharp.Parser.Constant<SyntaxToken>)
-    =
+        tw.Write($"{synTok.PositionedToken}({iT}<token>) '{tokenStr}'")
+    | TokenIndex.Virtual -> tw.Write($"{synTok.PositionedToken}(<virt>)")
+
+let printConstant (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (x: Constant<SyntaxToken>) =
     match x with
     | Constant.Literal value -> printTokenFull tw input lexed value
     | Constant.MeasuredLiteral(value, lAngle, measure, rAngle) -> failwith "Not implemented"
 
-let printPat (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (pat: XParsec.FSharp.Parser.Pat<SyntaxToken>) =
+let printPat (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (pat: Pat<SyntaxToken>) =
     match pat with
     | Pat.Const value ->
         tw.Write("Pat.Const: ")
@@ -50,12 +49,7 @@ let printPat (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (pat: XPars
         tw.WriteLine()
     | _ -> failwith "Not implemented"
 
-let printValueDefn
-    (tw: IndentedTextWriter)
-    (input: string)
-    (lexed: Lexed)
-    (valueDefn: XParsec.FSharp.Parser.ValueDefn<SyntaxToken>)
-    =
+let printValueDefn (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (valueDefn: ValueDefn<SyntaxToken>) =
     match valueDefn with
     | ValueDefn(mutableToken, access, pat, typarDefns, returnType, equals, expr) ->
         match mutableToken with
@@ -86,7 +80,7 @@ let printValueDefn
         printExpr tw input lexed expr
         tw.Indent <- tw.Indent - 1
 
-let printExpr (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (expr: XParsec.FSharp.Parser.Expr<SyntaxToken>) =
+let printExpr (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (expr: Expr<SyntaxToken>) =
     match expr with
     | Expr.Const value ->
         tw.Write("Const: ")
@@ -109,4 +103,32 @@ let printExpr (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (expr: XPa
         tw.Indent <- tw.Indent + 1
         printExpr tw input lexed body
         tw.Indent <- tw.Indent - 1
+    | Expr.InfixApp(left, op, right) ->
+        tw.Write("InfixApp: ")
+        printTokenMin tw input lexed op
+        tw.WriteLine()
+        tw.Indent <- tw.Indent + 1
+        printExpr tw input lexed left
+        printExpr tw input lexed right
+        tw.Indent <- tw.Indent - 1
+    | Expr.Tuple elements ->
+        tw.WriteLine("Tuple:")
+        tw.Indent <- tw.Indent + 1
+
+        for elem in elements do
+            printExpr tw input lexed elem
+
+        tw.Indent <- tw.Indent - 1
+    | Expr.List(lBracket, elements, rBracket) ->
+        tw.Write("List: ")
+        printTokenMin tw input lexed lBracket
+        tw.WriteLine()
+        tw.Indent <- tw.Indent + 1
+
+        for elem in elements do
+            printExpr tw input lexed elem
+
+        tw.Indent <- tw.Indent - 1
+        printTokenMin tw input lexed rBracket
+        tw.WriteLine()
     | _ -> failwith "Not implemented"
