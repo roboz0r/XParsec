b1c50a22d6ec2f9df4964a62e02436c0518b1982
roboz0r <mental.blindfolds.on@gmail.com>
2026-02-21 16:28:37 -0600

    Parse match expression

--- src/XParsec.FSharp/Debug.fs
+++ src/XParsec.FSharp/Debug.fs
@@ -139,6 +139,10 @@ let printPat (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (pat: Pat<S
         tw.Write("Pat.NamedSimple: ")
         printTokenFull tw input lexed ident
         tw.WriteLine()
+    | Pat.Wildcard underscore ->
+        tw.Write("Pat.Wildcard: ")
+        printTokenMin tw input lexed underscore
+        tw.WriteLine()
     | _ -> failwith "Not implemented"
 
 let printValueDefn (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (valueDefn: ValueDefn<SyntaxToken>) =
@@ -417,6 +421,48 @@ let printExpr (tw: IndentedTextWriter) (input: string) (lexed: Lexed) (expr: Exp
             printExpr tw input lexed elseExpr
             tw.Indent <- tw.Indent - 1
         | ValueNone -> ()
+
+        tw.Indent <- tw.Indent - 1
+    | Expr.Match(matchToken, matchExpr, withToken, rules) ->
+        tw.WriteLine("Match:")
+        tw.Indent <- tw.Indent + 1
+        tw.Write("MatchToken: ")
+        printTokenMin tw input lexed matchToken
+        tw.WriteLine()
+        tw.WriteLine("MatchExpr:")
+        tw.Indent <- tw.Indent + 1
+        printExpr tw input lexed matchExpr
+        tw.Indent <- tw.Indent - 1
+        tw.Write("WithToken: ")
+        printTokenMin tw input lexed withToken
+        tw.WriteLine()
+
+        let (Rules(firstBar, ruleList, bars)) = rules
+
+        for i in 0 .. ruleList.Length - 1 do
+            let bar = if i = 0 then firstBar else ValueSome bars[i - 1]
+
+            tw.WriteLine("Rule:")
+            tw.Indent <- tw.Indent + 1
+
+            match bar with
+            | ValueSome b ->
+                tw.Write("Bar: ")
+                printTokenMin tw input lexed b
+                tw.WriteLine()
+            | ValueNone -> ()
+
+            let (Rule(pat, guard, arrow, ruleExpr)) = ruleList[i]
+            tw.Write("Pat: ")
+            printPat tw input lexed pat
+            tw.Write("Arrow: ")
+            printTokenMin tw input lexed arrow
+            tw.WriteLine()
+            tw.WriteLine("Expr:")
+            tw.Indent <- tw.Indent + 1
+            printExpr tw input lexed ruleExpr
+            tw.Indent <- tw.Indent - 1
+            tw.Indent <- tw.Indent - 1
 
         tw.Indent <- tw.Indent - 1
     | _ -> failwithf "Not implemented %A" expr
