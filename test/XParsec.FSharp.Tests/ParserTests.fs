028b69833c4801543cf03faca2654ce1ebd22d0f
roboz0r <mental.blindfolds.on@gmail.com>
2026-02-21 11:10:51 -0600

    WIP handling #if directives

--- test/XParsec.FSharp.Tests/ParserTests.fs
+++ test/XParsec.FSharp.Tests/ParserTests.fs
@@ -65,4 +65,8 @@ let tests =
                 let path = Path.Combine(testDataDir.Value, "11_simple_if.fs")
                 testParseFile path
             }
+            ftest "If Directives" {
+                let path = Path.Combine(testDataDir.Value, "24_if_directive.fs")
+                testParseFile path
+            }
         ]
