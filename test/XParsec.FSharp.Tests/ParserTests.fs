7642a522fb022673793e45a837e6cb4d2f1a0d87
roboz0r <mental.blindfolds.on@gmail.com>
2026-02-21 16:39:23 -0600

    Refactoring tests

--- test/XParsec.FSharp.Tests/ParserTests.fs
+++ test/XParsec.FSharp.Tests/ParserTests.fs
@@ -12,98 +12,55 @@ open XParsec.CharParsers
 open XParsec.FSharp.Lexer
 open XParsec.FSharp.Parser
 
+/// Files (relative to testDataDir) that the parser can handle, together with any
+/// preprocessor symbols that must be defined.  Files that require features not yet
+/// implemented (e.g. interpolated strings) are intentionally omitted.
+let private parseTestFiles: (string * Set<string>) list =
+    [
+        "00_simple_expr.fs", Set.empty
+        "01_simple_arithmetic.fs", Set.empty
+        "02_simple_tuple.fs", Set.empty
+        "03_simple_list.fs", Set.empty
+        "04_simple_application.fs", Set.empty
+        "05_simple_array.fs", Set.empty
+        "06_simple_struct_tuple.fs", Set.empty
+        "07_simple_let.fs", Set.empty
+        "08_simple_begin_end.fs", Set.empty
+        "09_simple_long_ident.fs", Set.empty
+        "10_simple_typed_ident.fs", Set.empty
+        "11_simple_if.fs", Set.empty
+        "24_if_directive.fs", Set.empty
+        "24_if_directive.fs", Set.ofList [ "A" ]
+        "25_implicit_in.fs", Set.empty
+        "26_sequential.fs", Set.empty
+        "27_begin_end_seq.fs", Set.empty
+        "28_nested_lets.fs", Set.empty
+        "29_let_body_seq.fs", Set.empty
+        "30_let_in_body_seq.fs", Set.empty
+        "31_if_then_else_seq.fs", Set.empty
+        "32_match_seq.fs", Set.empty
+    ]
+
 [<Tests>]
 let tests =
     testList
         "ParserTests"
         [
-            test "Simple Expression" {
-                let path = Path.Combine(testDataDir.Value, "00_simple_expr.fs")
-                testParseFile path
-            }
-            test "Simple Arithmetic" {
-                let path = Path.Combine(testDataDir.Value, "01_simple_arithmetic.fs")
-                testParseFile path
-            }
-            test "Simple Tuple" {
-                let path = Path.Combine(testDataDir.Value, "02_simple_tuple.fs")
-                testParseFile path
-            }
-            test "Simple List" {
-                let path = Path.Combine(testDataDir.Value, "03_simple_list.fs")
-                testParseFile path
-            }
-            test "Simple Application" {
-                let path = Path.Combine(testDataDir.Value, "04_simple_application.fs")
-                testParseFile path
-            }
-            test "Simple Array" {
-                let path = Path.Combine(testDataDir.Value, "05_simple_array.fs")
-                testParseFile path
-            }
-            test "Simple Struct Tuple" {
-                let path = Path.Combine(testDataDir.Value, "06_simple_struct_tuple.fs")
-                testParseFile path
-            }
-            test "Simple Let" {
-                let path = Path.Combine(testDataDir.Value, "07_simple_let.fs")
-                testParseFile path
-            }
-            test "Simple Begin-End Block" {
-                let path = Path.Combine(testDataDir.Value, "08_simple_begin_end.fs")
-                testParseFile path
-            }
-            test "Simple Long Ident" {
-                let path = Path.Combine(testDataDir.Value, "09_simple_long_ident.fs")
-                testParseFile path
-            }
-            test "Simple Typed Ident" {
-                let path = Path.Combine(testDataDir.Value, "10_simple_typed_ident.fs")
-                testParseFile path
-            }
-            test "Simple If Expression" {
-                let path = Path.Combine(testDataDir.Value, "11_simple_if.fs")
-                testParseFile path
-            }
-            test "If Directives (inactive branch)" {
-                let path = Path.Combine(testDataDir.Value, "24_if_directive.fs")
-                testParseFile path
-            }
+            for (fileName, symbols) in parseTestFiles do
+                let path = Path.Combine(testDataDir.Value, fileName)
 
-            test "If Directives (active branch)" {
-                let path = Path.Combine(testDataDir.Value, "24_if_directive.fs")
-                testParseFileWithSymbols [ "A" ] path
-            }
-            test "Implicit In" {
-                let path = Path.Combine(testDataDir.Value, "25_implicit_in.fs")
-                testParseFile path
-            }
-            test "Sequential" {
-                let path = Path.Combine(testDataDir.Value, "26_sequential.fs")
-                testParseFile path
-            }
-            test "Begin-End Sequential" {
-                let path = Path.Combine(testDataDir.Value, "27_begin_end_seq.fs")
-                testParseFile path
-            }
-            test "Nested Lets" {
-                let path = Path.Combine(testDataDir.Value, "28_nested_lets.fs")
-                testParseFile path
-            }
-            test "Let Body Sequential" {
-                let path = Path.Combine(testDataDir.Value, "29_let_body_seq.fs")
-                testParseFile path
-            }
-            test "Let In-Body Sequential" {
-                let path = Path.Combine(testDataDir.Value, "30_let_in_body_seq.fs")
-                testParseFile path
-            }
-            test "If-Then-Else Sequential" {
-                let path = Path.Combine(testDataDir.Value, "31_if_then_else_seq.fs")
-                testParseFile path
-            }
-            test "Match Sequential" {
-                let path = Path.Combine(testDataDir.Value, "32_match_seq.fs")
+                let name =
+                    if symbols.IsEmpty then
+                        $"Parsing {fileName}"
+                    else
+                        let syms = symbols |> String.concat ","
+                        $"Parsing {fileName} ({syms})"
+
+                test name { testParseFileWith symbols path }
+
+            ptest "Temp test file" {
+                let fileName = "32_match_seq.fs"
+                let path = Path.Combine(testDataDir.Value, fileName)
                 testParseFile path
             }
         ]
