44716f3d18bd99def7289d5c7d68124ae8864f73
roboz0r <mental.blindfolds.on@gmail.com>
2026-02-21 23:01:40 -0600

    Fix parser test discovery

--- test/XParsec.FSharp.Tests/ParserTests.fs
+++ test/XParsec.FSharp.Tests/ParserTests.fs
@@ -4,7 +4,14 @@ open System.IO
 
 open Expecto
 
-/// Parser tests are discovered automatically from existing `.parsed` golden files.
+let specialFiles =
+    [
+        // These files require special handling because they contain `#if` directives
+        // that cause the parser to produce different outputs based on which symbols are defined.
+        "24_if_directive.fs", [ "A" ]
+    ]
+
+/// Parser tests are discovered automatically from `.fs` source files.
 /// To add a new test: create the `.fs` source file, run once locally (it will create the golden
 /// file and fail), verify the output is correct, then commit both files.
 /// To regenerate all golden files after a format change: set UPDATE_SNAPSHOTS=1 and re-run.
@@ -13,17 +20,17 @@ let tests =
     testList
         "ParserTests"
         [
-            for (path, symbols) in discoverParserTests testDataDir.Value do
+            for path in testData.Value do
                 let fileName = Path.GetFileName path
+                let name = $"Parsing {fileName}"
+                test name { testParseFile path }
 
-                let name =
-                    if symbols.IsEmpty then
-                        $"Parsing {fileName}"
-                    else
-                        let syms = symbols |> String.concat ","
-                        $"Parsing {fileName} ({syms})"
+            for (fileName, symbols) in specialFiles do
+                let path = Path.Combine(testDataDir.Value, fileName)
 
-                test name { testParseFileWith symbols path }
+                let name = $"""Parsing {fileName} ({String.concat ", " symbols})"""
+
+                test name { testParseFileWithSymbols symbols path }
         ]
 
 [<Tests>]
